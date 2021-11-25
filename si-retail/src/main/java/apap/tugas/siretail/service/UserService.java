93a2a3785802340199d6284b992ecd55df1ba85a
danielsyahputra13 <dnlshp@gmail.com>
2021-11-25 17:38:33 +0700

    Revert "feat: implement getListUser and updateUser service"
    This reverts commit 9debfc72a6405fddf34655f3f2289f6731214975.

--- si-retail/src/main/java/apap/tugas/siretail/service/UserService.java
+++ si-retail/src/main/java/apap/tugas/siretail/service/UserService.java
@@ -1,10 +1,4 @@
 package apap.tugas.siretail.service;
 
-import apap.tugas.siretail.model.UserModel;
-
-import java.util.List;
-
 public interface UserService {
-    List<UserModel> getListUser();
-    UserModel updateUserByUsername(String username);
 }
--- si-retail/src/main/java/apap/tugas/siretail/service/UserServiceImpl.java
+++ si-retail/src/main/java/apap/tugas/siretail/service/UserServiceImpl.java
@@ -1,24 +1,4 @@
 package apap.tugas.siretail.service;
 
-import apap.tugas.siretail.model.UserModel;
-import apap.tugas.siretail.repository.UserDb;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class UserServiceImpl implements UserService{
-    @Autowired
-    private UserDb userDb;
-
-    @Override
-    public List<UserModel> getListUser() {
-        return userDb.findAll();
-    }
-
-    @Override
-    public UserModel updateUserByUsername(String username) {
-        return null;
-    }
+public class UserServiceImpl {
 }
