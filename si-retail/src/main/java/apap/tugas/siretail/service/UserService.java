0eccc615766ee28bf890f1321e4561d80f51861b
ayeshathalia <79743153+ayeshathalia@users.noreply.github.com>
2021-11-25 20:50:45 +0700

    Ayesha (#5)
    * feat: mencoba membuat fitur login dan logout
    
    * feat: memperbaiki fitur login
    
    Co-authored-by: Ayesha Thalia <ayeshaputri588@gmail.com>
    Co-authored-by: Daniel Syahputra <57356926+danielsyahputra13@users.noreply.github.com>

--- si-retail/src/main/java/apap/tugas/siretail/service/UserService.java
+++ si-retail/src/main/java/apap/tugas/siretail/service/UserService.java
@@ -1,4 +1,8 @@
 package apap.tugas.siretail.service;
 
+import apap.tugas.siretail.model.UserModel;
+
 public interface UserService {
+    UserModel addUser(UserModel user);
+    String encrypt(String password);
 }
