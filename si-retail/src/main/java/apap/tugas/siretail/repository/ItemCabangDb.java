6647b476c0cb7d8bbdb3d83d721d47d80041b798
danielsyahputra13 <dnlshp@gmail.com>
2021-11-29 23:26:10 +0700

    feat: membuat service cabang

---
+++ si-retail/src/main/java/apap/tugas/siretail/repository/ItemCabangDb.java
@@ -0,0 +1,8 @@
+package apap.tugas.siretail.repository;
+
+import apap.tugas.siretail.model.ItemCabangModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemCabangDb extends JpaRepository<ItemCabangModel, Long> {
+
+}
--- si-retail/src/main/java/apap/tugas/siretail/service/CabangServiceImpl.java
+++ si-retail/src/main/java/apap/tugas/siretail/service/CabangServiceImpl.java
@@ -1,4 +1,16 @@
 package apap.tugas.siretail.service;
 
-public class CabangServiceImpl {
+import apap.tugas.siretail.model.CabangModel;
+import apap.tugas.siretail.repository.CabangDb;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CabangServiceImpl implements CabangService{
+    @Autowired
+    private CabangDb cabangDb;
+
+
+    @Override
+    public CabangModel addCabang(CabangModel cabang) {
+        return cabangDb.save(cabang);
+    }
 }
