2bcaa22eef9c1892e6061672ae687588c364607e
danielsyahputra13 <dnlshp@gmail.com>
2021-12-06 12:47:12 +0700

    feat: implementasi consumer untuk list kupon

--- si-retail/src/main/java/apap/tugas/siretail/service/KuponRestService.java
+++ si-retail/src/main/java/apap/tugas/siretail/service/KuponRestService.java
@@ -1,5 +1,9 @@
 package apap.tugas.siretail.service;
 
+import apap.tugas.siretail.rest.KuponDetail;
+import reactor.core.publisher.Mono;
+
 public interface KuponRestService {
     void terapkanPromoKeItem(Long idItem);
+    Mono<String> getListKupon();
 }
--- si-retail/src/main/java/apap/tugas/siretail/service/KuponRestServiceImpl.java
+++ si-retail/src/main/java/apap/tugas/siretail/service/KuponRestServiceImpl.java
@@ -1,22 +1,35 @@
 package apap.tugas.siretail.service;
 
 import apap.tugas.siretail.model.ItemCabangModel;
+import apap.tugas.siretail.rest.Setting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
 public class KuponRestServiceImpl implements KuponRestService{
-    @Autowired
-    ItemCabangService itemCabangService;
+    private final WebClient webClient;
+
+    public KuponRestServiceImpl(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(Setting.kuponUrl).build();
+    }
 
     @Autowired
-    KuponRestService kuponRestService;
+    ItemCabangService itemCabangService;
 
     @Override
     public void terapkanPromoKeItem(Long idItem) {
         ItemCabangModel item = itemCabangService.getItemByIdItem(idItem);
+    }
+
+    @Override
+    public Mono<String> getListKupon() {
+        return this.webClient.get().uri("/rest/coupon/list")
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
