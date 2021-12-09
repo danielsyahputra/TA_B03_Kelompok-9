3f247cba334904c9adc6a5035deb701102741811
ayeshathalia <79743153+ayeshathalia@users.noreply.github.com>
2021-12-09 21:07:30 +0700

    Feat: Memperbaiki fitur 16 (fixed) (#22)
    Co-authored-by: Ayesha Thalia <ayeshaputri588@gmail.com>

--- si-retail/src/main/java/apap/tugas/siretail/security/WebSecurityConfig.java
+++ si-retail/src/main/java/apap/tugas/siretail/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,6 +36,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/api/**");
     }
 
     @Autowired
